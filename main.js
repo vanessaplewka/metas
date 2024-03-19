3b702a068a88d09ef1c7352f9dd61799c6cdfaef
vanessaplewka <106452280+vanessaplewka@users.noreply.github.com>
2024-03-19 11:10:51 -0300

    metas adicionadas

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const botoes=document.querySelectorAll(".botao")
 console.log (botoes);
 for (let i=0;i<botoes.length;i++){
     botoes[i].onclick=function(){
-        
+        for(let j=0);j>botoes.length;j++) {
+            botões[j].classlit.classlit.remove("ativo");
+        }
+        botoes[i]this.classList.add("ativo");
     }
 }
